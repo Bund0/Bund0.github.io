356afc0c0129671a842abe587552fa91762d62d4
Bund0 <54556522+Bund0@users.noreply.github.com>
2024-11-19 19:57:26 +0100

    Add files via upload

--- js/SlideShow.js
+++ js/SlideShow.js
@@ -1,4 +1,4 @@
-let slideIndex = 1;
+ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -28,3 +28,4 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
